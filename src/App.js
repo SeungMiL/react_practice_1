efa573e16e1635e3bdc7f69a23140eed18ea5100
SeungMiL <tmskqj@naver.com>
2022-01-22 15:33:00 +0900

    react_practice_4

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
+
+
 function App() {
 
   let [writeTitle, tranceTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '인덕원 등갈비 맛집']);
   let [like, likeTrance] = useState(0);
   let [modal, modalTrance] = useState(false);
+  let [pressTitle, pressTrance] = useState(0);
+  let [inputVal, valChange] = useState()
+
 
   function changeTitle() {
     let newArray = [...writeTitle];
@@ -15,15 +20,15 @@ function App() {
     tranceTitle(newArray);
   }
 
-  function arrayUi(){
+  // function arrayUi(){
 
-    let arrayNew = []
-    for (let i = 0; i <3; i++) {
-      arrayNew.push(<div>안녕</div>);
+  //   let arrayNew = []
+  //   for (let i = 0; i <3; i++) {
+  //     arrayNew.push(<div>안녕</div>);
       
-    }
-    return arrayNew
-  }
+  //   }
+  //   return arrayNew
+  // }
 
 
 
@@ -48,19 +53,22 @@ function App() {
         <p>2월 05일 발행</p>
         <hr />
       </div>
-      {arrayUi()}
+      {/* <button onClick={()=>{ pressTrance(0)}}>버튼1</button>
+      <button onClick={()=>{ pressTrance(1)}}>버튼2</button>
+      <button onClick={()=>{ pressTrance(2)}}>버튼3</button> */}
+      {/* {arrayUi()}  */}
 
       {
         modal === true
-          ? <Modal></Modal>
+          ? <Modal writeTitle={writeTitle} pressTitle={pressTitle}></Modal>
           : null
       }
 
       {
-        writeTitle.map(function (i) {
+        writeTitle.map(function (i , a) {
           return (
-            <div className="list">
-              <h3> {i} </h3>
+            <div className="list" key={a}>
+              <h3 onClick={()=>{ pressTrance(a)}}> {i} </h3>
               <p>2월 12일 발행</p>
               <hr />
             </div>
@@ -69,16 +77,29 @@ function App() {
 
       }
 
+      <input onChange={(e)=>{ valChange(e.target.value)}}/>
+
+      {inputVal}
+
+      <div className="publish">
+        <input onChange={(e)=>{ valChange(e.target.value) }}/>
+        <button onClick={()=>{
+          let arrayCopy = [...writeTitle];
+          arrayCopy.unshift(inputVal);
+          tranceTitle(arrayCopy);
+        }}>저장</button>
+      </div>
+
 
 
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
-      <h2>제목</h2>
+      <h2>제목 {props.writeTitle[props.pressTitle]}</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
